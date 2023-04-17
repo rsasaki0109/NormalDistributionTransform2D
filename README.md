2ede459578f520b0363c516bc84784fc63d41db3
Ryohei Sasaki <rsasaki0109@gmail.com>
2023-04-17 20:33:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Python sample code of 2D NDT Scan Matching
+This Python script demonstrates a 2D scan matching algorithm called Normal Distributions Transform (NDT) for aligning two point clouds. NDT is commonly used in robotics for tasks like localization and mapping. The script generates synthetic point clouds, transforms one of them, and tries to recover the true transformation parameters by matching the transformed point cloud to the original.
+
+## Usage
+To run the script, simply execute the following command in your terminal:
+
+```bash
+python script/normal_distributions_transform.py
+```
+.
+## Results
+After running the script, a plot will be displayed showing the source points (red), target points (blue), and transformed source points (green). The true and estimated transformation parameters will also be printed.
+
+![Result](image/result.png)
+
+
+
---
+++ script/normal_distributions_transform.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
+def generate_src_points(n_points, field_length):
+    px = (np.random.rand(n_points) - 0.5) * field_length
+    py = (np.random.rand(n_points) - 0.5) * field_length
+    return np.vstack((px, py)).T
+
+def apply_transformation(points, transform):
+    R = np.array([[np.cos(transform[2]), -np.sin(transform[2])],
+                  [np.sin(transform[2]), np.cos(transform[2])]])
+    return points @ R.T + transform[:2]
+
+def add_noise(points, scale):
+    return points + np.random.normal(scale=scale, size=points.shape)
+
+def ndt_match(src_points, dst_points, resolution=1.0, max_iter=50, tol=1e-6):
+    src_points = np.asarray(src_points)
+    dst_points = np.asarray(dst_points)
+
+    # Build a grid of cells with the specified resolution.
+    min_corner = np.min(dst_points, axis=0) - resolution
+    max_corner = np.max(dst_points, axis=0) + resolution
+    grid_shape = np.ceil((max_corner - min_corner) / resolution).astype(int)
+    grid = np.zeros(grid_shape, dtype=[('mean', float, 2), ('cov', float, (2, 2)), ('count', int)])
+
+    # Fill the grid with destination points data.
+    for p in dst_points:
+        cell_idx = tuple(((p - min_corner) / resolution).astype(int))
+        grid[cell_idx]['count'] += 1
+        grid[cell_idx]['mean'] += (p - grid[cell_idx]['mean']) / grid[cell_idx]['count']
+        grid[cell_idx]['cov'] += np.outer(p - grid[cell_idx]['mean'], p - grid[cell_idx]['mean'])
+
+    # Calculate the covariance matrices for non-empty cells.
+    for cell in grid.flat:
+        if cell['count'] > 1:
+            cell['cov'] /= cell['count'] - 1
+        else:
+            cell['cov'] = np.eye(2) * resolution
+
+    # Optimization function to minimize.
+    def objective_func(x):
+        R = np.array([[np.cos(x[2]), -np.sin(x[2])], [np.sin(x[2]), np.cos(x[2])]])
+        transformed_points = src_points @ R.T + x[:2]
+
+        cost = 0.0
+        for p in transformed_points:
+            cell_idx = tuple(((p - min_corner) / resolution).astype(int))
+            if 0 <= cell_idx[0] < grid_shape[0] and 0 <= cell_idx[1] < grid_shape[1]:
+                cell = grid[cell_idx]
+                if cell['count'] > 0:
+                    d = p - cell['mean']
+                    cost += d @ np.linalg.pinv(cell['cov']) @ d
+
+        return cost
+
+    # Optimize the transformation parameters.
+    x0 = np.zeros(3)
+    res = minimize(objective_func, x0, method='BFGS', options={'maxiter': max_iter, 'gtol': tol})
+    return res.x
+
+def plot_results(src_points, dst_points, src_points_transformed):
+    plt.scatter(src_points[:, 0], src_points[:, 1], c='r', label='Source Points')
+    plt.scatter(dst_points[:, 0], dst_points[:, 1], c='b', label='Target Points')
+    plt.scatter(src_points_transformed[:, 0], src_points_transformed[:, 1], c='g', label='Transformed Source Points')
+    plt.legend()
+    plt.axis('equal')
+    plt.title('2D NDT Scan Matching')
+    plt.show()
+
+def main():
+    n_points = 1000
+    field_length = 50.0
+
+    src_points = generate_src_points(n_points, field_length)
+    true_transform = np.array([0.1, 0.2, np.pi / 12])
+    dst_points = apply_transformation(src_points, true_transform)
+
+    estimated_transform = ndt_match(src_points, dst_points)
+
+    src_points_transformed = apply_transformation(src_points, estimated_transform)
+
+    plot_results(src_points, dst_points, src_points_transformed)
+
+    print(f"True transform: {true_transform}")
+    print(f"Estimated transform: {estimated_transform}")
+
+if __name__ == '__main__':
+    main()
