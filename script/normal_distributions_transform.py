6ef7ed871530645a57dd0a2ab3c4ad1939c68a34
Ryohei Sasaki <rsasaki0109@gmail.com>
2023-04-19 06:06:48 +0900

    Add objective_func_vectorized

--- script/normal_distributions_transform.py
+++ script/normal_distributions_transform.py
@@ -15,7 +15,7 @@ def apply_transformation(points, transform):
 def add_noise(points, scale):
     return points + np.random.normal(scale=scale, size=points.shape)
 
-def ndt_match(src_points, dst_points, resolution=10, max_iter=20, tol=1e-5):
+def ndt_match(src_points, dst_points, resolution=7, max_iter=20, tol=1e-5):
     src_points = np.asarray(src_points)
     dst_points = np.asarray(dst_points)
 
@@ -55,9 +55,29 @@ def ndt_match(src_points, dst_points, resolution=10, max_iter=20, tol=1e-5):
 
         return cost
 
+    def objective_func_vectorized(x):
+        R = np.array([[np.cos(x[2]), -np.sin(x[2])], [np.sin(x[2]), np.cos(x[2])]])
+        transformed_points = src_points @ R.T + x[:2]
+
+        cell_idx = np.floor((transformed_points - min_corner) / resolution).astype(int)
+        valid_mask = np.all(np.logical_and(cell_idx >= 0, cell_idx < grid_shape), axis=1)
+        valid_cell_idx = cell_idx[valid_mask]
+
+        valid_cells = grid[valid_cell_idx[:, 0], valid_cell_idx[:, 1]]
+        count_mask = valid_cells['count'] > 0
+        valid_cells = valid_cells[count_mask]
+        valid_transformed_points = transformed_points[valid_mask][count_mask]
+
+        d = valid_transformed_points - valid_cells['mean']
+        cov_inv = np.linalg.pinv(valid_cells['cov'])
+        cost = np.sum(np.einsum('...ij,...j->...i', d @ cov_inv, d))
+
+        return cost
+
+
     # Optimize the transformation parameters.
     x0 = np.zeros(3)
-    res = minimize(objective_func, x0, method='BFGS', options={'maxiter': max_iter, 'gtol': tol})
+    res = minimize(objective_func_vectorized, x0, method='BFGS', options={'maxiter': max_iter, 'gtol': tol})
     return res.x
 
 def plot_results(src_points, dst_points, src_points_transformed):
